2a984002ba52759e50e072d03c079550e977e9ae
Yusup Narkuvatov <ynn.131095@gmail.com>
2025-03-08 11:32:39 +0300

    test image upload to laravel cloud........

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -215,12 +215,13 @@
                             <span
                                 class="flex flex-col items-start flex-shrink-0 h-full ml-2 leading-none translate-y-px">
                                 <span>{{auth()->user()->name}}
-                                    @isset($filename)
-                                        {{$filename}}
-                                    @endisset ($file)
+
 
                                 </span>
-                                <span class="text-xs font-light text-neutral-400">{{auth()->user()->email}}</span>
+                                <span class="text-xs font-light text-neutral-400">{{auth()->user()->email}}
+                                    @if($filename)
+                                        {{$filename}}
+                                    @endif</span>
                             </span>
 
                         </button>
